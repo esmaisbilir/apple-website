754522f0149fd84b11092d822e4052fe819f23b1
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-09-09 14:10:29 +0300

    edit ModelView

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,45 +1,43 @@
-import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
-
-import * as THREE from 'three'
-import Lights from "./Lights"
-import IPhone from "./IPhone"
-import { Suspense } from "react"
+import { Canvas } from '@react-three/fiber'; // Canvas @react-three/fiber'den gelir
+import { OrbitControls, PerspectiveCamera, Html } from "@react-three/drei";
+import * as THREE from 'three';
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <View
-      index={index}
-      id={gsapType}
-      className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
-    >
-      {/* Ambient Light */}
-      <ambientLight intensity={0.3} />
+    <div className={`w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}>
+      <Canvas>
+        {/* Ambient Light */}
+        <ambientLight intensity={0.3} />
 
-      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-      <Lights />
+        <Lights />
 
-      <OrbitControls 
-        makeDefault
-        ref={controlRef}
-        enableZoom={false}
-        enablePan={false}
-        rotateSpeed={0.4}
-        target={new THREE.Vector3(0, 0, 0)}
-        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-      />
-      
-      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0, 0, 0]}>
-        <Suspense fallback={<div>Loading</div>}>
-          <IPhone 
-            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-            item={item}
-            size={size}
-          />
-        </Suspense>
-      </group>
-    </View>
-  )
-}
+        <OrbitControls 
+          makeDefault
+          ref={controlRef}
+          enableZoom={false}
+          enablePan={false}
+          rotateSpeed={0.4}
+          target={new THREE.Vector3(0, 0, 0)}
+          onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+        />
+        
+        <group ref={groupRef} name={index === 1 ? 'small' : 'large'} position={[0, 0, 0]}>
+          <Suspense fallback={<Html><div>Loading...</div></Html>}>
+            <IPhone 
+              scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+              item={item}
+              size={size}
+            />
+          </Suspense>
+        </group>
+      </Canvas>
+    </div>
+  );
+};
 
-export default ModelView
+export default ModelView;
